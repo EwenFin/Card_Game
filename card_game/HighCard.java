812d2a1860a414fd581e2011e845f54f54440680
Ewen Carr <ewencarr@hotmail.com>
2017-03-11 11:26:26 +0000

    Highcard: can add players to game

---
+++ card_game/HighCard.java
@@ -0,0 +1,21 @@
+package card_game;
+import java.util.*;
+
+public class HighCard {
+  public Deck deck;
+  public ArrayList<Player> players;
+
+  public HighCard(){
+    this.deck = deck;
+    this.players = new ArrayList<Player>();
+  }
+
+  public int playerCount(){
+    return players.size();
+  }
+
+  public void addPlayerToGame(Player player){
+    players.add(player);
+  }
+
+}
---
+++ specs/HighCardTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import card_game.*;
+
+
+public class HighCardTest{
+
+  HighCard game;
+
+  Deck deck;
+
+  Player player1;
+  Player player2;
+  Player player3;
+
+  Hand player1hand;
+  Hand player2hand;
+  Hand player3hand;
+
+@Before
+public void before(){
+  deck = new Deck();
+  player1hand = new Hand();
+  player2hand = new Hand();
+  player3hand = new Hand();
+  player1 = new Player("Ewen", player1hand);
+  player2 = new Player("Steven", player2hand);
+  player3 = new Player("Oli", player3hand);
+  game = new HighCard();
+
+}
+
+@Test
+public void canAddPlayersToGame(){
+  game.addPlayerToGame(player1);
+  game.addPlayerToGame(player2);
+  game.addPlayerToGame(player3);
+  assertEquals(3, game.playerCount());
+
+}
+
+
+
+
+
+}
